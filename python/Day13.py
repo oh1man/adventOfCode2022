3676f2833f1cffecdd69cce8ddbedc37c158fd69
Adam Ohman <adam.ohman@vermiculus.se>
2022-12-13 13:42:10 +0100

    WIP

--- python/Day13.py
+++ python/Day13.py
@@ -1,18 +1,32 @@
 from utils.Stopwatch import runWithStopwatch
 
 
-def compare(left, right):
-    # If integers, left < right
-    # If lists, left.size <= right.size
-    # If list and integer => make integer to list and run list comparison
-    pass
+def isCorrectOrder(left, right, withLeftRetry = False, withRightRetry = False):
+    for i in range(0, len(left)):
+        lefty = left[0] if withLeftRetry else left[i]
+        if not withRightRetry and i == len(right):
+            return False
+        righty = right[0] if withRightRetry else right[i]
+        if isinstance(lefty, int) and isinstance(righty, int):
+            if lefty > righty:
+                return False
+        elif isinstance(lefty, list) and isinstance(righty, list):
+            if not isCorrectOrder(lefty, righty):
+                return False
+        elif isinstance(lefty, list) and isinstance(righty, int):
+            if not isCorrectOrder(lefty, [righty], withRightRetry=True):
+                return False
+        elif isinstance(lefty, int) and isinstance(righty, list):
+            if not isCorrectOrder([lefty], righty, withLeftRetry=True):
+                return False
+    return True
 
 
 def parse(line):
     packet = []
     number = ''
     jump_to = 0
-    for i in range(1, len(line) - 1):
+    for i in range(1, len(line)):
         char = line[i]
         if i <= jump_to:
             continue
@@ -24,7 +38,6 @@ def parse(line):
             case "]":
                 if number != '':
                     packet.append(int(number))
-                    number = ''
                 return packet, i
             case ",":
                 if number == '':
@@ -33,18 +46,22 @@ def parse(line):
                 number = ''
             case _:
                 number += char
-    return packet
+    return packet, i
 
 
 def part1():
     text_input = open("../input/day13.txt", "r").readlines()
+    correctPairs = []
+    pair = 0
     for i in range(0, len(text_input), 3):
+        pair += 1
         left = text_input[i]
-        left, _ = parse(left)
+        left, _ = parse(left.strip())
         right = text_input[i + 1]
-        right, _ = parse(right)
-        if compare(left, right):
-            pass
+        right, _ = parse(right.strip())
+        if isCorrectOrder(left, right):
+            correctPairs.append(pair)
+    print("Part1: " + str(sum(correctPairs)))
 
 
 if __name__ == '__main__':
