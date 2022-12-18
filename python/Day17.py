750daacd3172ee1a5228b55f3f956509d2263024
oh1man <adam.ohman2@gmail.com>
2022-12-18 20:42:24 +0100

    Day 17 Python: Part 1

--- python/Day17.py
+++ python/Day17.py
@@ -4,40 +4,63 @@ from utils.Stopwatch import runWithStopwatch
 def part1():
     jetstream = open("../input/day17_test.txt", "r").read().strip()
     chamber = Chamber(7, jetstream, 2, 3)
-    for i in range(10):
+    for i in range(2022):
         chamber.drop()
-    print("Part1: " + str(1), end="\n")
+    print("Part1: " + str(chamber.getHeight()), end="\n")
 
 # This is the main simulator class
 # Owns the physics for the dropping.
 class Chamber:
     def __init__(self, width, jetstream, leftDiff, verticalDiff):
+        self.j = 0
         self.width = width
+        self.rightWall = 8
+        self.leftWall = 0
         self.jetstream = jetstream
         self.rocks: list[tuple] = []
         self.rockGenerator = RockGenerator()
         self.leftDiff = leftDiff
         self.verticalDiff = verticalDiff
+        self.floor = []
+        self.floorLevel = 0
+        for i in range(1, width + 1):
+            self.floor.append((i, self.floorLevel))
 
     def drop(self):
-        bot = self.getHeight() + self.verticalDiff
-        left = self.leftDiff
+        bot = self.getHeight() + self.verticalDiff + 1
+        left = self.leftWall + self.leftDiff + 1
         rock = self.rockGenerator.getNextRock(left, bot)
+        falling = True
+        while falling:
+            # Step sideways
+            stream = self.jetstream[self.j]
+            match stream:
+                case ">":
+                    if not rock.isLeftOf(self.rightWall, self.rocks):
+                        rock.moveRight()
+                case "<":
+                    if not rock.isRightOf(self.leftWall, self.rocks):
+                        rock.moveLeft()
+            self.j += 1
+            if self.j == len(self.jetstream):
+                self.j = 0
 
-        # TODO: What height do we start at?
-        # Do the simulation?
-        print("test")
-        # TODO: When at rest save the coordingates in the chamber
-        for coor in rock.coordinates:
-            self.rocks.append(coor)
+            # Step down
+            if not rock.isOver(self.floor, self.rocks):
+                rock.moveDown()
+            else:
+                falling = False
+
+        for el in rock.coordinates:
+            self.rocks.append(el)
+        self.rocks.sort(key=lambda x: x[1])
 
     def getHeight(self):
         if len(self.rocks) == 0:
-            return 0
+            return self.floorLevel
         else:
-            self.rocks.sort(key=lambda p: p[1])
+            self.rocks.sort(key=lambda p: p[1], reverse=True)
             return self.rocks[0][1]
-
 
 
 class Rock:
@@ -48,28 +71,70 @@ class Rock:
         """
         Moves the rock coordinates down.
         """
+        under = self.getCoordinatesUnder()
+        for i in range(len(under)):
+            self.coordinates[i] = under[i]
+
+    def getCoordinatesUnder(self):
+        under = []
         for i in range(len(self.coordinates)):
             x = self.coordinates[i][0]
             y = self.coordinates[i][1]
-            self.coordinates[i] = (x, y - 1)
+            under.append((x, y - 1))
+        return under
+
+    def isOver(self, floor, rocks):
+        """
+        Checks if coordinates is under the rock formation
+        """
+        under = self.getCoordinatesUnder()
+        return self.isIn(under, floor) or self.isIn(under, rocks)
 
     def moveRight(self):
         """
         Moves the rock coordinates to the right.
         """
+        right = self.getCoordinatesToRight()
+        for i in range(len(right)):
+            self.coordinates[i] = right[i]
+
+    def getCoordinatesToRight(self):
+        right = []
         for i in range(len(self.coordinates)):
             x = self.coordinates[i][0]
             y = self.coordinates[i][1]
-            self.coordinates[i] = (x + 1, y)
+            right.append((x + 1, y))
+        return right
+
+    def isLeftOf(self, wall, rocks):
+        right = list(map(lambda t: t[0], self.getCoordinatesToRight()))
+        return wall in right or self.isIn(right, rocks)
 
     def moveLeft(self):
         """
         Moves the rock coordinates to the left.
         """
+        left = self.getCoordinatesToLeft()
+        for i in range(len(left)):
+            self.coordinates[i] = left[i]
+
+    def getCoordinatesToLeft(self):
+        left = []
         for i in range(len(self.coordinates)):
             x = self.coordinates[i][0]
             y = self.coordinates[i][1]
-            self.coordinates[i] = (x - 1, y)
+            left.append((x - 1, y))
+        return left
+
+    def isRightOf(self, wall, rocks):
+        left = list(map(lambda t: t[0], self.getCoordinatesToLeft()))
+        return wall in left or self.isIn(left, rocks)
+
+    def isIn(self, points, rocks):
+        for el in rocks:
+            if el in points:
+                return True
+        return False
 
 
 class RockGenerator:
